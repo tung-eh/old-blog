baadf7d5ecb8a31b9bb79bd0a17575c62b25a2da
Tung Teng <thetungctn@gmail.com>
2019-08-02 08:50:22 +0700

    Dynamic pages from markdown files

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,41 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require("path")
 
-// You can delete this file if you're not using it
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return new Promise((resolve, reject) => {
+    const PostTemplate = path.resolve("src/templates/post.js")
+
+    resolve(
+      graphql(`
+        {
+          allMarkdownRemark {
+            edges {
+              node {
+                frontmatter {
+                  path
+                }
+              }
+            }
+          }
+        }
+      `).then(result => {
+        if (result.errors) {
+          console.log(result.errors)
+          reject(result.errors)
+        }
+        result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+          const { path } = node.frontmatter
+
+          createPage({
+            path,
+            component: PostTemplate,
+            context: {
+              pathSlug: path,
+            },
+          })
+          resolve()
+        })
+      })
+    )
+  })
+}
---
+++ src/templates/post.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+
+const Post = ({ data }) => {
+  const { markdownRemark } = data;
+  const { title, date } = markdownRemark.frontmatter;
+  const { html } = markdownRemark;
+
+  return (
+    <div>
+        <div >
+          <h1>{title}</h1>
+          <div>
+            <p>{date}</p>
+          </div>
+          <div
+            dangerouslySetInnerHTML={{ __html: html }}
+          />
+        </div>
+    </div>
+  );
+};
+
+export default Post;
+
+export const query = graphql`
+  query($pathSlug: String!) {
+    markdownRemark(frontmatter: { path: { eq: $pathSlug } }) {
+      html
+      frontmatter {
+        title
+        date
+      }
+    }
+  }
+`;
