aacae6854f134b07972ee371165ce8e7f0972909
Tung Teng <thetungctn@gmail.com>
2019-10-06 20:03:42 +0700

    Config font

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,11 +4,19 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name:  `posts`,
-        path: `${__dirname}/content/posts`
-      }
+        name: `posts`,
+        path: `${__dirname}/content/posts`,
+      },
     },
     `gatsby-plugin-styled-components`,
+    {
+      resolve: "gatsby-plugin-web-font-loader",
+      options: {
+        google: {
+          families: ["IBM+Plex+Mono"],
+        },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
